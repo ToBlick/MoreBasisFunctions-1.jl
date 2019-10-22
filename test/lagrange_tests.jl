d4a56a2d428f30503806cf01ca2fa4f8e558d704
Michael Kraus <michael.kraus@ipp.mpg.de>
2019-10-22 15:20:32 +0200

    restructure Lagrange polynomial tests

--- test/lagrange_tests.jl
+++ test/lagrange_tests.jl
@@ -1,51 +1,56 @@
 
 using BasisFunctions: hasderivative, hasantiderivative
 
-ξ = [-1.0, +1.0]
-b = Lagrange(ξ)
 P = 80
 
-@testset "$(rpad("Basic functionality",P))" begin
-    @test nodes(b) == ξ
-    @test nnodes(b) == 2
-    @test degree(b) == 1
-    @test hasderivative(b) == true
-    @test hasantiderivative(b) == true
-    @test support(b) == LagrangeInterval{eltype(ξ)}()
-end
+@testset "$(rpad("Lagrange polynomials",P))" begin
+
+    ξ = [-1.0, +1.0]
+    b = Lagrange(ξ)
+
+    @testset "$(rpad("Basic functionality",P))" begin
+        @test nodes(b) == ξ
+        @test nnodes(b) == 2
+        @test degree(b) == 1
+        @test hasderivative(b) == true
+        @test hasantiderivative(b) == true
+        @test support(b) == LagrangeInterval{eltype(ξ)}()
+    end
 
 
-@testset "$(rpad("Basis evaluation",P))" begin
-    @test b[1](-1.0) == 1.0
-    @test b[1]( 0.0) == 0.5
-    @test b[1](+1.0) == 0.0
+    @testset "$(rpad("Basis evaluation",P))" begin
+        @test b[1](-1.0) == 1.0
+        @test b[1]( 0.0) == 0.5
+        @test b[1](+1.0) == 0.0
 
-    @test b[2](-1.0) == 0.0
-    @test b[2]( 0.0) == 0.5
-    @test b[2](+1.0) == 1.0
-end
-
-
-@testset "$(rpad("Basis derivative",P))" begin
-    @test eval_element_derivative(b, 1, -1.0) == -0.5
-    @test eval_element_derivative(b, 1,  0.0) == -0.5
-    @test eval_element_derivative(b, 1, +1.0) == -0.5
-
-    @test eval_element_derivative(b, 2, -1.0) == +0.5
-    @test eval_element_derivative(b, 2,  0.0) == +0.5
-    @test eval_element_derivative(b, 2, +1.0) == +0.5
-end
+        @test b[2](-1.0) == 0.0
+        @test b[2]( 0.0) == 0.5
+        @test b[2](+1.0) == 1.0
+    end
 
 
-@testset "$(rpad("Basis antiderivative",P))" begin
-    @test moment(b, 1) == 1.0
-    @test moment(b, 2) == 1.0
+    @testset "$(rpad("Basis derivative",P))" begin
+        @test eval_element_derivative(b, 1, -1.0) == -0.5
+        @test eval_element_derivative(b, 1,  0.0) == -0.5
+        @test eval_element_derivative(b, 1, +1.0) == -0.5
 
-    @test eval_element_antiderivative(b, 1, -1.0) == 0.0
-    @test eval_element_antiderivative(b, 1,  0.0) == 0.75
-    @test eval_element_antiderivative(b, 1, +1.0) == 1.0
+        @test eval_element_derivative(b, 2, -1.0) == +0.5
+        @test eval_element_derivative(b, 2,  0.0) == +0.5
+        @test eval_element_derivative(b, 2, +1.0) == +0.5
+    end
 
-    @test eval_element_antiderivative(b, 2, -1.0) == 0.0
-    @test eval_element_antiderivative(b, 2,  0.0) == 0.25
-    @test eval_element_antiderivative(b, 2, +1.0) == 1.0
+
+    @testset "$(rpad("Basis antiderivative",P))" begin
+        @test moment(b, 1) == 1.0
+        @test moment(b, 2) == 1.0
+
+        @test eval_element_antiderivative(b, 1, -1.0) == 0.0
+        @test eval_element_antiderivative(b, 1,  0.0) == 0.75
+        @test eval_element_antiderivative(b, 1, +1.0) == 1.0
+
+        @test eval_element_antiderivative(b, 2, -1.0) == 0.0
+        @test eval_element_antiderivative(b, 2,  0.0) == 0.25
+        @test eval_element_antiderivative(b, 2, +1.0) == 1.0
+    end
+
 end
